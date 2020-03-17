c495619fd54f100ebe0523994860c4e21bc8f91a
walypadilla <walter04padilla@gmail.com>
2020-03-17 13:17:20 -0600

    segundo commit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -10,41 +13,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
- 
-app.get('/usuario', function (req, res) {
-    res.json('get usuario');
-});
+// import rutas de usuario
+app.use( require('./routes/usuario') );
 
-app.post('/usuario', function (req, res) {
-
-    let body = req.body;
-
-    if( body.nombre === undefined ) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-    
-});
-
-app.put('/usuario/:id', function (req, res) {
-
-    let id = req.params.id;
-
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function (req, res) {
-    res.json('delete usuario');
-});
 
 app.listen(process.env.PORT, () => {
-    console.log('Escuhando el puerto, 3000');
+    console.log('Escuhando el puerto: ', process.env.PORT);
 });
+
+
+mongoose.connect(process.env.URLDB,
+    {useNewUrlParser: true, useCreateIndex: true}).
+    then(
+        () => { console.log('Base de Datos ONLINE'); },
+        (err) => { throw new Error(err) }
+)
+        
+
+   
+
