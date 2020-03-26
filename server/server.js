e947bf1dfa3988d8f81b3381405a6a97133ce1ad
walypadilla <walter04padilla@gmail.com>
2020-03-26 14:44:46 -0600

    variable de entorno MONGO

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const app = express();
 const bodyParser = require('body-parser');
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded( { extended: false } ));
  
 // parse application/json
-app.use(bodyParser.json());
+app.use( bodyParser.json() );
 
 // import rutas de usuario
 app.use( require('./routes/usuario') );
@@ -23,7 +23,7 @@ app.listen(process.env.PORT, () => {
 
 
 mongoose.connect(process.env.URLDB,
-    {useNewUrlParser: true, useCreateIndex: true}).
+    {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true}).
     then(
         () => { console.log('Base de Datos ONLINE'); },
         (err) => { throw new Error(err) }
